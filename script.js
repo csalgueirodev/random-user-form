44f94844e696fd57a55bcb7a85e74539e0a7b0be
César Salgueiro <csalgueiro.dev@gmail.com>
2021-05-28 19:03:52 +0200

    Adds animateText function to animate an element with text Adds animateInputValue function to animate the attribute value Adds tpying animation to text Adds closures to settimeout loops

--- script.js
+++ script.js
@@ -1,46 +1,90 @@
+const API_URL = 'https://randomuser.me/api/?inc=gender,name,dob,phone,picture,login,email,location';
 const btn = document.querySelector("#btn-generator")
 const progress = document.querySelector(".progress")
 const span = document.querySelector(".progress > span")
-const animationTime = 100;
+const title = document.querySelector(".card-title > h1")
+const animationTime = 200;
+let animateInputs = false;
 
 fetchRandomUser();
+animateText(title);
+animateText(btn);
 
 btn.addEventListener("click", fetchRandomUser)
 
 function fetchRandomUser() {
-    fetch('https://randomuser.me/api/?inc=gender,name,dob,phone,picture,login,email,location')
+    fetch()
         .then(response => response.json())
         .then(data => {
-            const userData = apiToUserData(data.results[0]);
+            const userData = apiToUserData(data.results[0])
             Object.keys(userData).forEach(key => {
-                setValueToInput(key, userData);
-            });
-            animateGrid();
-        });
+                setValueToInput(key, userData)
+            })
+            animateGrid()
+            animateInputs = !animateInputs
+        })
 }
 
 function animateGrid() {
     const divs = document.querySelectorAll(".grid > div")
-    divs.forEach((element, i) => {
-        element.style.display = 'none';
-        setTimeout(function () {
-            element.style.display = '';
-            element.classList.add("animated", "fadeInUp");
-        }, i * animationTime)
-    });
+    divs.forEach((el, idx) => {
+        (function (element, i) {
+            element.style.display = 'none'
+            return setTimeout(function () {
+                element.style.display = ''
+                element.classList.add("animated", "fadeInUp")
+
+                animateText(element.querySelector("label"));
+            }, i * (animationTime / 2))
+        }(el, idx))
+    })
+}
+
+
+function animateText(element) {
+    const stringText = element.textContent
+    element.textContent = ""
+    const arrayText = stringText.split("")
+    let text = "";
+    arrayText.forEach((el, idx) => {
+        (function (i) {
+            return setTimeout(function () {
+                text += arrayText[i]
+                element.textContent = text
+            }, i * animationTime)
+        }(idx))
+    })
+}
+
+function animateInputValue(input, value) {
+    const arrayText = value.toString().split("")
+    let text = "";
+
+    arrayText.forEach((el, idx) => {
+        (function (i) {
+            return setTimeout(function () {
+                text += arrayText[i]
+                input.setAttribute("value", text)
+            }, i * (animationTime / 4))
+        }(idx))
+    })
 }
 
 function setValueToInput(key, userData) {
     const input = document.querySelector(`input[name=${key}]`)
     if (input != null) {
         if (input.getAttribute("type") === "radio") {
-            const radios = [...document.querySelectorAll(`[name=${key}]`)]
+            const radios = document.querySelectorAll(`[name=${key}]`)
             radios.forEach(r => {
-                r.setAttribute("checked", false);
+                r.setAttribute("checked", false)
             })
             document.querySelector(`[name=${key}][value=${userData[key]}]`).checked = true
         } else {
-            input.setAttribute("value", userData[key])
+            if (animateInputs) {
+                animateInputValue(input, userData[key])
+            } else {
+                input.setAttribute("value", userData[key])
+            }
         }
     } else {
         if (key === "picture") {
@@ -61,11 +105,11 @@ function apiToUserData(randomuser) {
                 userData["address"] = `${randomuser[key].street?.name} ${randomuser[key].street?.number}, ${randomuser[key].city}. ${randomuser[key].postcode} ${randomuser[key].country}`
                 break
             case "dob":
-                userData["age"] = randomuser[key].age;
+                userData["age"] = randomuser[key].age
                 break
             case "login":
-                userData["password"] = randomuser[key].password;
-                userData["username"] = randomuser[key].username;
+                userData["password"] = randomuser[key].password
+                userData["username"] = randomuser[key].username
                 break
             case "picture":
                 userData[key] = randomuser[key].large;
@@ -74,6 +118,6 @@ function apiToUserData(randomuser) {
                 userData[key] = randomuser[key]
                 break
         }
-    });
+    })
     return userData
 }
